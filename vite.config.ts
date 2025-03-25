baa2e88c6d67ba67899cac58d4991436ac1e7034
Ojolowo <ojolowoboluwatife1@gmail.com>
2025-03-25 08:39:13 +0100

    ignore typescript error on config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+//@ts-expect-error ///
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -8,7 +9,9 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
+      //@ts-expect-error jjj
       '@': path.resolve(__dirname, 'src', 'app'),
+      //@ts-expect-error jjj
       app: path.resolve(__dirname, 'src', 'app'),
     },
   },
