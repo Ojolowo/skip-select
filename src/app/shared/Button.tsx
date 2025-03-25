401754a47b3e78aee2b586240232737b79c9ad27
Ojolowo <ojolowoboluwatife1@gmail.com>
2025-03-25 11:43:14 +0100

    use ButtonHTMLAttributes instead

--- src/app/shared/Button.tsx
+++ src/app/shared/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import classNames from 'classnames';
 
-interface Props extends React.HTMLAttributes<HTMLButtonElement> {
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   size?: 'sm' | 'default';
   variant?: 'secondary' | 'primary';
   disabled?: boolean;
